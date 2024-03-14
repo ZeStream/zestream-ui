1028b64c748956066a3e36bce5d51f3353389b4f
shubhangi0905 <shubhangisrivastava0905@gmail.com>
2024-03-14 21:19:57 +0530

    Login function completed

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,29 +2,32 @@
 // styles
 import styles from "./Header.module.css"
 
+import Link from "next/link";
+import { Button } from "@radix-ui/themes";
 // widgets
 import { Avatar, Flex, IconButton, Text } from "@radix-ui/themes";
 
 // icons
 import { SunIcon, MoonIcon } from '@radix-ui/react-icons'
 import { ThemeMode, useThemeStore } from "@/context/ThemeState";
-
-
 export default function Header() {
     const { theme, toggleTheme } = useThemeStore();
-
     const renderThemeIcon = () => {
         if (theme === ThemeMode.LIGHT) {
             return <SunIcon />
         }
         return <MoonIcon />
     }
-
     return (
         <header className={styles.container}>
             <Text as="p">
                 ZeStream
             </Text>
+            <Button>
+        <Link href="/login">
+            Login
+            </Link>
+            </Button>
 
             <Flex gap="3">
                 <IconButton onClick={toggleTheme}>
